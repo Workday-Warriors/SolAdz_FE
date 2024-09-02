e7096d3b7e004f640ae6ecad4d8034aa64b86a88
M Afzaal Afzal <afzaal1721@gmail.com>
2024-09-02 22:39:02 +0500

    refactor: add link to logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { ConnectWalletButton } from "./common/ConnectWalletButton";
 import TopLogo from "../assets/top-logo.png";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const rightText = "MY SOLADZ RANK: STARTER | MY SOL ADDRESS:";
@@ -7,12 +8,12 @@ export const Header = () => {
   return (
     <div>
       <header className="flex gap-1 justify-between items-center mt-12">
-        <div className="flex items-center gap-2 sm:gap-4">
+        <Link to={"/"} className="flex items-center gap-2 sm:gap-4">
           <img className="max-w-[50px] lg:max-w-[62px] w-full" src={TopLogo} alt="top logo" />
-          <h1 className="bg-gradient-to-br from-[#F7F6FF] via-[#F7F6FF_26%] to-[#C669FF] bg-clip-text text-[30px] lg:text-[50px] text-transparent poller">
+          <h2 className="bg-gradient-to-br from-[#F7F6FF] via-[#F7F6FF_26%] to-[#C669FF] bg-clip-text text-[30px] lg:text-[50px] text-transparent poller">
             SolAdz
-          </h1>
-        </div>
+          </h2>
+        </Link>
 
         <div className="flex items-center text-xs lg:text-sm">
           <div className="hidden md:flex">
