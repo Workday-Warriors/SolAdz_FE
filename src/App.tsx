3673953f4657be99efb2606bcb9755063374a1d7
M Afzaal Afzal <afzaal1721@gmail.com>
2024-09-04 18:25:53 +0500

    feat: add loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CryptoInnovationPage } from "./views/CryptoInnovationPage";
 import { MainPage } from "./views/MainPage";
+import Lottie from "lottie-react";
+import animationData from "./assets/loading/loading.json";
+import { useEffect, useState } from "react";
 
 const router = createBrowserRouter([
   {
@@ -16,7 +19,35 @@ const router = createBrowserRouter([
 
 // Main App component
 function App() {
-  return <RouterProvider router={router} />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div
+        className={`${
+          isLoading ? "opacity-100" : "opacity-0"
+        } transition-all duration-300 absolute min-h-screen w-full h-full top-0 left-0 bg-blue-400 z-[99999] pointer-events-none`}
+      >
+        <div className="h-full w-full flex items-center justify-center">
+          <Lottie
+            className="max-w-[800px]"
+            height={500}
+            width={500}
+            animationData={animationData}
+          />
+        </div>
+      </div>
+      <RouterProvider router={router} />;
+    </div>
+  );
 }
 
 export default App;
